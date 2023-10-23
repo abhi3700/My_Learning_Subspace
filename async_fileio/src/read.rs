d986a1b50adde15001b2d3c9aafa2f5862e6e212
abhi3700 <dalvath3700@gmail.com>
2023-10-24 00:13:49 +0530

    added more notes on read async fileio workspace member

--- async_fileio/src/read.rs
+++ async_fileio/src/read.rs
@@ -1,9 +1,17 @@
 //! Handle CRUD operations asynchronous in a file
 //! - [x] Read
 //!
-//! Here, we can open the file using either of these methods:
+//! NOTE: Before opening a file in order to read information,
+//! we need to give read access when opened a file. And then
+//! we can use the read methods to read it entirely or partially
+//!
+//! Here, we can open the file using either of these methods with `read` permission:
 //! - `File::open()`
-//! - `OpenOptions::new()`
+//! - `OpenOptions::new()`. It has an edge over the former method as it gives multiple permissions
+//!     at a time via chains like this...
+//!     - 'r': `OpenOptions::new().read(true)`
+//!     - 'rw': `OpenOptions::new().read(true).write(true)`
+//!     - 'rw': `OpenOptions::new().write(true)`
 //!
 //! We can also read the file:
 //! - entirely via `read_to_end`
@@ -15,7 +23,7 @@ use tokio::io::AsyncReadExt;
 use tokio::io::Result;
 
 pub(crate) async fn main() -> Result<()> {
-    // opening a file in read-only mode.
+    // opening a file in read-only mode as permission
     // let mut file = File::open("demo.txt").await?;    // M-1
     let mut file = OpenOptions::new().read(true).open("demo.txt").await?; // M-2
 
